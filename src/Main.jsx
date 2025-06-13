565eeb62a2ed913fad0db9f60872260fbbaf11b9
Garima Gupta <moonlight@Garimas-MacBook-Air.local>
2025-06-13 10:45:51 +0530

    Fix: updated main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import { UserDataProvider } from './userData';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//       <UserDataProvider>
-//         <App />
-//       </UserDataProvider>
-//   </React.StrictMode>
-// )
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // ✅ Make sure this file exists
