7efd59cd7c94d2576b8b4cd6df7cf69f035063b2
Garima Gupta <moonlight@Garimas-MacBook-Air.local>
2025-06-13 10:26:10 +0530

    Fix: added @vitejs/plugin-react for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()], // Enable React plugin
-//   server: {
-//     port: 5174, // Match the port you're using
-//     host: '0.0.0.0', // Allow access from network (optional, helps with local testing)
-//     hmr: {
-//       protocol: 'ws', // Explicitly use WebSocket
-//       host: 'localhost',
-//       port: 5174,
-//       clientPort: 5174, // Ensure client connects to the same port
-//     },
-//   },
-// });
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
